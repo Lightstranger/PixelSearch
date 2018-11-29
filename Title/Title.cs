bcf79f0de58f5c2dbcbb340f266c5e3f1740713f
Lightstranger <40529278+Lightstranger@users.noreply.github.com>
2018-11-28 20:45:54 -0700

    Update Title.cs

--- Title/Title.cs
+++ Title/Title.cs
@@ -5,6 +5,8 @@ namespace PixelSearch.Title {
     [System.Runtime.InteropServices.ComVisible(false)]
     public sealed class TitleScreen : MarshalByRefObject, IDisposable, System.Drawing.IDeviceContext {
         private void DrawBackground(PaintEventArgs e) {
+            // Error: "(7,37): error CS0246: The type or namespace name 'PaintEventArgs 
+            // could not be found (are you missing a using directive or an assembly reference?)"
             Image Background = new Image.FromFile("TitleBackground.png");
             PointF BackgroundCorner = new PointF(0.0F, 0.0F);
             e.Graphics.DrawImage(Background, BackgroundCorner);
